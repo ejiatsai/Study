2b219d91fc7e77d21657a34566ff120fa9cebb25
ejiatsai <cyixie464@gmail.com>
2025-12-08 22:21:35 +0800

    hahaha

--- 1208PR/pr1.c
+++ 1208PR/pr1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct contact{
     char name[105][20];
@@ -35,22 +36,59 @@ int main(){
                 printf("%s, %s",Contact.name[j],Contact.phone[j]);
                 printf("\n");
             }
+            puts("--");
         }
         else if(action == 'l'){
             char Search_phone[11];
+            int count = 0;
             scanf("%s",Search_phone);
             for(int j = 0;j < i;j++){
                 if(strcmp(Contact.phone[j],Search_phone) == 0){
                     printf("%s\n",Contact.name[j]);
+                    break;
+                }
+                count++;
+            }
+            if(count == i){
+                printf("This phone number does not exist\n");
+            }
+            puts("--");
+        }
+        else if(action == 'n'){
+            char tmp_name[20];
+            char tmp_phone[11];
+            for(int x = 0;x < i - 1;x++){
+                for(int y = 0;y < i - 1 - x;y++){
+                    if(strcmp(Contact.name[y],Contact.name[y + 1]) > 0){
+                        strcpy(tmp_name,Contact.name[y]);
+                        strcpy(tmp_phone,Contact.phone[y]);
+                        strcpy(Contact.name[y],Contact.name[y + 1]);
+                        strcpy(Contact.phone[y],Contact.phone[y + 1]);
+                        strcpy(Contact.name[y + 1],tmp_name);
+                        strcpy(Contact.phone[y + 1],tmp_phone);
+                    }
                 }
             }
         }
-        else if(action == 'n'){
-            
+        else if(action == 'p'){
+            char tmp_name[20];
+            char tmp_phone[11];
+            for(int x = 0;x < i - 1;x++){
+                for(int y = 0;y < i - 1 - x;y++){
+                    if(strcmp(Contact.phone[y],Contact.phone[y + 1]) > 0){
+                        strcpy(tmp_name,Contact.name[y]);
+                        strcpy(tmp_phone,Contact.phone[y]);
+                        strcpy(Contact.name[y],Contact.name[y + 1]);
+                        strcpy(Contact.phone[y],Contact.phone[y + 1]);
+                        strcpy(Contact.name[y + 1],tmp_name);
+                        strcpy(Contact.phone[y + 1],tmp_phone);
+                    }
+                }
+            }
         }
         else {
-
+            printf("This is wrong action\n");
+            puts("--");
         }
-        puts("--");
     }
 }
