b242a6608c5fa486ff284876af6500757330fb05
ejiatsai <cyixie464@gmail.com>
2025-12-15 16:57:12 +0800

    hahaha

--- 1201HW/hw2.c
+++ 1201HW/hw2.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
-#define MAXN 100005
 
 int main(){
     int n = 0, q = 0;
-    char Process_name[MAXN][11] = {'\0'};
-    int Process_time[MAXN] = {'\0'};
+    char Process_name[100000][11] = {'\0'};
+    int Process_time[100000] = {'\0'};
     int Sum_of_time = 0;
     int Num_of_downprocess = 0;
     scanf("%d", &n);
     scanf("%d", &q);
-    int Process_id[MAXN] = {'\0'};
     for(int i = 0; i < n; i++){
         scanf("%s", Process_name[i]);
         scanf("%d", &Process_time[i]);
-        Process_id[i] = i;
     }
-    int head = 0,tail = n - 1;
+    int Process_count = 0;
     while(Num_of_downprocess < n){
-        if(Process_time[Process_id[head]] > q){
-            Process_time[Process_id[head]] -= q;
-            head = (head + 1) % n;
-            tail = (tail + 1) % n;
+        if(Process_time[Process_count] == 0){
+            Process_count = (Process_count + 1) % n;
+            continue;
+        }
+        if(Process_time[Process_count] > q){
+            Process_time[Process_count] -= q;
             Sum_of_time += q;
         }
         else{
-            Sum_of_time += Process_time[Process_id[head]];
-            printf("%s %d\n", Process_name[Process_id[head]], Sum_of_time);
-            head = (head + 1) % n;
+            Sum_of_time += Process_time[Process_count];
+            printf("%s %d\n", Process_name[Process_count], Sum_of_time);
+            Process_time[Process_count] = 0;
             Num_of_downprocess++;
         }
+        Process_count = (Process_count + 1) % n;
     }
 }
